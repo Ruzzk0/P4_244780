8f0d063cce18e6f75f8dd344e074a6153a92acd7
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2025-02-05 00:16:24 -0700

    se termino absolutamente todas las vistas
    cada vista ya quedo como debe de ser, con cada color y todo lo que se necesitaba

--- Practica4_244780/app/src/main/java/cervantes/fedra/practica4_/MainActivity.kt
+++ Practica4_244780/app/src/main/java/cervantes/fedra/practica4_/MainActivity.kt
@@ -1,6 +1,8 @@
 package cervantes.fedra.practica4_
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
+        val button:Button = findViewById(R.id.button_getStarted) as Button
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
