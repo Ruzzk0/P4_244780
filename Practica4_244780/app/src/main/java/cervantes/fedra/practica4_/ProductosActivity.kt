16d87e07ec15a53598b868abe26f2b75d8a17e60
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2025-02-11 23:46:11 -0700

    codigo
    se implementa mas codigo funcional en kotlin para su vista y funcion

--- Practica4_244780/app/src/main/java/cervantes/fedra/practica4_/ProductosActivity.kt
+++ Practica4_244780/app/src/main/java/cervantes/fedra/practica4_/ProductosActivity.kt
@@ -1,51 +1,87 @@
-package cervantes.fedra.practica4_
+package cervantes.fedra.practica4
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cervantes.fedra.practica4_.Product
+import cervantes.fedra.practica4_.R
+
 
 class ProductosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_productos)
+        agregaProductos()
     }
 
-    var coldDrinks =  ArrayList<Product>()
+    var coldDrinks = ArrayList<Product>()
+    var hotDrinks = ArrayList<Product>()
+    var sweets = ArrayList<Product>()
+    var salties = ArrayList<Product>()
 
-    fun agregaProductos(){
-        coldDrinks.add(Product(“caramel Frap”, R.drawable.caramelfrap, “Caramel syrup meets coffee, milk and ice and whipped cream and buttery caramel sauce layer the love on top.”, 5))
-        coldDrinks.add(Product(“Chocolate Frap”, R.drawable.chocolatefrap, “Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash.”, 6))
-        coldDrinks.add(Product(“Cold Brew”, R.drawable.coldbrew, “Created by steeping medium-to-coarse ground coffee in room temperature water for 12 hours or longer.”, 3))
-        coldDrinks.add(Product(“Matcha Latte”, R.drawable.matcha, “Leafy taste of matcha green tea powder with creamy milk and a little sugar for a flavor balance that will leave you feeling ready and raring to go.”, 4))
-        coldDrinks.add(Product(“Oreo Milkshake”, R.drawable.oreomilkshake, “Chocolate ice cream, and oreo cookies. Topped with whipped cream with cocoa and chocolate syrup.”, 7))
-        coldDrinks.add(Product(“Peanut Milkshake”, R.drawable.peanutmilkshake, “Vanilla ice cream, mixed with peanut butter and chocolate.”, 7))
+    fun agregaProductos() {
+        coldDrinks.add(Product("Caramel Frap", R.drawable.caramelfrap, "Caramel syrup meets coffee, milk and ice and whipped cream and buttery caramel sauce layer the love on top.", 5))
+        coldDrinks.add(Product("Chocolate Frap", R.drawable.chocolatefrap, "Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash.", 6))
+        coldDrinks.add(Product("Cold Brew", R.drawable.coldbrew, "Created by steeping medium-to-coarse ground coffee in room temperature water for 12 hours or longer.", 3))
+        coldDrinks.add(Product("Matcha Latte", R.drawable.matcha, "Leafy taste of matcha green tea powder with creamy milk and a little sugar for a flavor balance that will leave you feeling ready and raring to go.", 4))
+        coldDrinks.add(Product("Oreo Milkshake", R.drawable.oreomilkshake, "Chocolate ice cream, and oreo cookies. Topped with whipped cream with cocoa and chocolate syrup.", 7))
+        coldDrinks.add(Product("Peanut Milkshake", R.drawable.peanutmilkshake, "Vanilla ice cream, mixed with peanut butter and chocolate.", 7))
 
-        var hotDrinks = ArrayList<Product>()
-        hotDrinks.add(Product(“Latte”, R.drawable.latte, “Coffee drink made with espresso and steamed milk”, 6))
-        hotDrinks.add(Product(“Hot chocolate”, R.drawable.hotchocolate, “Heated drink consisting of shaved chocolate, topped with marshmallows.”, 5))
-        hotDrinks.add(Product(“Espresso”, R.drawable.espresso, “Full-flavored, concentrated form of coffee.”, 4))
-        hotDrinks.add(Product(“Chai Latte”, R.drawable.chailatte, “Spiced tea concentrate with milk”, 6))
-        hotDrinks.add(Product(“Capuccino”, R.drawable.capuccino, “A cappuccino is an espresso-based coffee drink, prepared with steamed foam.”, 7))
-        hotDrinks.add(Product(“American coffee”, R.drawable.americano, “Espresso with hot water”, 2))
+        hotDrinks.add(Product("Latte", R.drawable.latte, "Coffee drink made with espresso and steamed milk", 6))
+        hotDrinks.add(Product("Hot Chocolate", R.drawable.hotchocolate, "Heated drink consisting of shaved chocolate, topped with marshmallows.", 5))
+        hotDrinks.add(Product("Espresso", R.drawable.espresso, "Full-flavored, concentrated form of coffee.", 4))
+        hotDrinks.add(Product("Chai Latte", R.drawable.chailatte, "Spiced tea concentrate with milk", 6))
+        hotDrinks.add(Product("Cappuccino", R.drawable.capuccino, "A cappuccino is an espresso-based coffee drink, prepared with steamed foam.", 7))
+        hotDrinks.add(Product("American Coffee", R.drawable.americano, "Espresso with hot water", 2))
 
-        var sweets = ArrayList<Product>()
-        sweets.add(Product(“Blueberry cake”, R.drawable.blueberrycake, “Vanilla cake flavor, topped with cheese topping and blueberries.”, 6))
-        sweets.add(Product(“Chocolate cupcake”, R.drawable.chocolatecupcake, “Chocolate cupcakes topped with butter cream and cherries”, 3))
-        sweets.add(Product(“Lemon tartalette”, R.drawable.lemontartalette, “Pastry shell with a lemon flavored filling”, 4))
-        sweets.add(Product(“Red Velvet cake”, R.drawable.redvelvetcake, “Soft, moist, buttery cake topped with an easy cream cheese frosting.”, 6))
-        sweets.add(Product(“Cherry cheesecake”, R.drawable. strawberrycheesecake, “This cherry topped cheesecake is positively creamy and delicious and will be your new favorite dessert.”, 7))
-        sweets.add(Product(“Tiramisu”, R.drawable.tiramisu, “Coffee-flavored Italian dessert”, 6))
+        sweets.add(Product("Blueberry Cake", R.drawable.blueberrycake, "Vanilla cake flavor, topped with cheese topping and blueberries.", 6))
+        sweets.add(Product("Chocolate Cupcake", R.drawable.chocolatecupcake, "Chocolate cupcakes topped with butter cream and cherries", 3))
+        sweets.add(Product("Lemon Tartalette", R.drawable.lemontartalette, "Pastry shell with a lemon flavored filling", 4))
+        sweets.add(Product("Red Velvet Cake", R.drawable.redvelvetcake, "Soft, moist, buttery cake topped with an easy cream cheese frosting.", 6))
+        sweets.add(Product("Cherry Cheesecake", R.drawable.strawberrycheesecake, "This cherry topped cheesecake is positively creamy and delicious and will be your new favorite dessert.", 7))
+        sweets.add(Product("Tiramisu", R.drawable.tiramisu, "Coffee-flavored Italian dessert", 6))
 
-        var salties = ArrayList<Product>()
-        salties.add(Product(“Chicken crepes”, R.drawable.chickencrepes, “Fine crepes stuffed with Alfredo chicken, spinach and mushrooms.”, 6))
-        salties.add(Product(“Club Sandwich”, R.drawable.clubsandwich, “A delicious sandwich served with french fries.”, 5))
-        salties.add(Product(“Panini”, R.drawable.panini, “Sandwich made with Italian bread  served warmed by grilling.”, 4))
-        salties.add(Product(“Philly cheese steak”, R.drawable. phillycheesesteak, “Smothered in grilled onions, green peppers, mushrooms, and Provolone.”, 6))
-        salties.add(Product(“Nachos”, R.drawable. nachos, “Tortilla chips layered with beef and   melted cheddar cheese. Served with fried beans, guacamole, pico de gallo, and sour topping.”,  7))
-
+        salties.add(Product("Chicken Crepes", R.drawable.chickencrepes, "Fine crepes stuffed with Alfredo chicken, spinach and mushrooms.", 6))
+        salties.add(Product("Club Sandwich", R.drawable.clubsandwich, "A delicious sandwich served with french fries.", 5))
+        //salties.add(Product("Panini", R.drawable.panini, "Sandwich made with Italian bread served warmed by grilling.", 4))
+        salties.add(Product("Philly Cheese Steak", R.drawable.phillycheesesteak, "Smothered in grilled onions, green peppers, mushrooms, and Provolone.", 6))
+        salties.add(Product("Nachos", R.drawable.nachos, "Tortilla chips layered with beef and melted cheddar cheese. Served with fried beans, guacamole, pico de gallo, and sour topping.", 7))
     }
 
+    private class AdaptadorProductos:BaseAdapter{
+        var productos = ArrayList<Product>()
+        var contexto: Context? = null
+
+        constructor(contexto: Context, productos: ArrayList<Product> ){
+            this.productos = productos
+            this.contexto = contexto
+        }
+
+        override fun getCount(): Int {
+            return productos.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return productos[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var prod = productos[position]
+            var inflador = LayoutInflater.from(contexto)
+            var visita = inflador.inflate(R.layout.producto_view, null)
+        }
+
+    }
 }
