22ab48abc4e1933f086c5aaf0382cad3f2ffca8f
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2025-02-11 23:29:24 -0700

    añadiendo todo
    codigo de la parte de kotlin

---
+++ Practica4_244780/app/src/main/java/cervantes/fedra/practica4_/Product.kt
@@ -0,0 +1,8 @@
+package cervantes.fedra.practica4_
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: double){
+
+}
